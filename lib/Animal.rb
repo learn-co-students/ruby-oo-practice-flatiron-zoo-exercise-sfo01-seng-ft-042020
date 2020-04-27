67cd1feaea981366e3ab611a26eb2ce05f7c407d
tharveybrown <tharveybrown@gmail.com>
2020-04-27 13:22:58 -0700

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,16 @@
 class Animal
+  attr_accessor  :weight, :zoo
+  attr_reader :species, :nickname
+  @@all = []
+  
+  def initialize(species, weight, nickname)
+    @species = species
+    @weight = weight 
+    @nickname = nickname
+    @@all << self
+  end
 
+  def self.all
+    @@all 
+  end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,12 @@ require 'pry'
 
 #Test your code here
 
-
+an_a = Animal.new("Dog", 15, "fido")
+an_b = Animal.new("Cat", 33, "fido")
+an_c = Animal.new("Dog", 22, "marc")
+an_e = Animal.new("Zebra", 33, "stripe")
+zoo_a = Zoo.new("bax", "sf")
+zoo_c = Zoo.new("mar", "sf")
+zoo_b = Zoo.new("lago", "nyc")
 binding.pry
 puts "done"
