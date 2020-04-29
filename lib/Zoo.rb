01e131683e191ebe81c13cadac6171bf166c8578
stellarwobble <jennidemuir@gmail.com>
2020-04-29 14:56:22 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,57 @@
 class Zoo
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize (name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_animal(animal)
+        animal.zoo = self
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end
+
+    def animal_species
+        species_array = Animal.all.select do |animal|
+            animal.zoo == self
+        end
+        species_array.map do |animal|
+            animal.species
+        end.uniq
+        
+    end
+
+    def find_by_species(species)
+        animals.select do |animal|
+            animal.species == species
+        end
+
+     end
+
+    def animal_nickname
+        animals.map do |animal|
+            animal.nickname 
+        end
+    end
+
+    def self.find_by_location(location)
+        @@all.select do |zoo|
+            zoo.location == location
+        end
+    end
+
+
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,32 @@ require 'pry'
 
 #Test your code here
 
+zoo1 = Zoo.new("sf zoo", "sf")
+zoo2 = Zoo.new("sj zoo", "sj")
+zoo3 = Zoo.new("shity zoo", "sf")
+
+lion1 = Animal.new("lion", "66", "lionboi")
+lion2 = Animal.new("lion", "300", "liondaddy")
+lion3 = Animal.new("lion", "142", "simba")
+
+bear1 = Animal.new("bear", "200", "bearbb")
+bear2 = Animal.new("bear", "166", "bearma")
+bear3 = Animal.new("bear", "222", "yogi")
+
+deer1 = Animal.new("deer", "111", "bambi")
+deer2 = Animal.new("deer", "115", "hornsby")
+deer3 = Animal.new("deer", "125", "ants")
+
+zoo1.add_animal(lion1)
+zoo1.add_animal(lion2)
+zoo1.add_animal(bear1)
+zoo1.add_animal(bear2)
+zoo1.add_animal(deer1)
+zoo1.add_animal(deer2)
+
+zoo2.add_animal(lion3)
+zoo2.add_animal(bear3)
+zoo2.add_animal(deer3)
 
 binding.pry
 puts "done"
