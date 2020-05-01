4b2ae68e6ad6f59f5cac594d44353d5414e76201
github username <you@example.com>
2020-04-30 22:29:36 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,31 @@
 class Zoo
+    attr_accessor :name, :location
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map {|animal| animal.species}.uniq
+    end
+
+    def animal_nicknames
+        animals.map {|animal| animal.nickname}
+    end
+
+    def self.find_by_location(location)
+        self.all.select {|zoo| zoo.location == location}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,20 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
-
 #Test your code here
+zoo1 = Zoo.new("San Fran Zoo", "San Francisco")
+zoo2 = Zoo.new("Marin Zoo", "Marin")
+zoo3 = Zoo.new("San Jose Zoo", "San Jose")
+zoo4 = Zoo.new("Sacramento Zoo", "Sacramento")
+zoo5 = Zoo.new("SacZoo", "Sacramento")
 
+animal1 = Animal.new("elephen", 1000, "Elly", zoo1)
+animal2 = Animal.new("giraffe", 800, "Raffy", zoo1)
+animal3 = Animal.new("tiger", 500, "Tigger", zoo2)
+animal4 = Animal.new("lion", 500, "Lonny", zoo2)
+animal5 = Animal.new("bird", 20, "Birdy", zoo3)
+animal6 = Animal.new("monkey", 200, "Paul", zoo4)
+animal7 = Animal.new("penguin", 100, "Penny", zoo4)
 
 binding.pry
 puts "done"
